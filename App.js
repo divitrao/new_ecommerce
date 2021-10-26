a69c97496d549c4b8d5e9927753f36dc4d795bfa
Amit <mailmeamit27@gmail.com>
2021-10-26 18:13:18 +0530

    Final Commit

--- App.js
+++ App.js
@@ -1,22 +1,17 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import { Text, View } from 'react-native';
 import HomeStack from './src/navigations/otp_to_home_navigator/stackNavigator';
 import { NavigationContainer } from '@react-navigation/native';
-import { LogBox } from 'react-native';
+
 
 
 const App=({navigation}) => {
-  LogBox.ignoreLogs(["Require cycle:"])
-  LogBox.ignoreLogs(['VirtualizedLists should never be nested',])
+
 console.log(navigation)
   return (
     
     <NavigationContainer>
-    
-      
-      <HomeStack />
-    
+     <HomeStack />
     </NavigationContainer>
     
   );
